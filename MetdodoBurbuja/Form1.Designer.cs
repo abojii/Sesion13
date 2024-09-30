880c1d27d81dc78601f4ac31baedbd8ce55be666
abojii <abojorgec@gmail.com>
2024-09-30 10:22:39 -0600

    Agregar archivos de proyecto.

---
+++ MetdodoBurbuja/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace MetdodoBurbuja
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.tbNumber = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lbNumbers = new System.Windows.Forms.ListBox();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(24, 66);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(151, 20);
+            this.lblNumero.TabIndex = 0;
+            this.lblNumero.Text = "Ingrese un numero: ";
+            // 
+            // tbNumber
+            // 
+            this.tbNumber.Location = new System.Drawing.Point(203, 66);
+            this.tbNumber.Name = "tbNumber";
+            this.tbNumber.Size = new System.Drawing.Size(139, 26);
+            this.tbNumber.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(378, 55);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(134, 43);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // lbNumbers
+            // 
+            this.lbNumbers.FormattingEnabled = true;
+            this.lbNumbers.ItemHeight = 20;
+            this.lbNumbers.Location = new System.Drawing.Point(77, 196);
+            this.lbNumbers.Name = "lbNumbers";
+            this.lbNumbers.Size = new System.Drawing.Size(435, 284);
+            this.lbNumbers.TabIndex = 3;
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(378, 123);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(134, 39);
+            this.btnSort.TabIndex = 4;
+            this.btnSort.Text = "Ordenar";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 492);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.lbNumbers);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.tbNumber);
+            this.Controls.Add(this.lblNumero);
+            this.Name = "Form1";
+            this.Text = "Metodo de Burbuja";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.TextBox tbNumber;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lbNumbers;
+        private System.Windows.Forms.Button btnSort;
+    }
+}
+
---
+++ MetdodoBurbuja/models/Bubble.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetdodoBurbuja.models
+{
+    internal class Bubble
+    {
+        public void BubbleSort(int[] array)
+        {
+            try
+            {
+                for(int i=0; i<array.Length; i++)
+                {
+                    for(int j= i+1; j<array.Length; j++)
+                    {
+                        if(array[i] == array[j])
+                        {
+                            int temp = array[j];
+                            array[j] = array[i];
+                            array[i] = temp;
+                        }
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
